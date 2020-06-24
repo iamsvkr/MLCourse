6f5b7248c113f8c910c17549a5a367efef4f8d23
iamsvkr <shivamkumarrajput56789@gmail.com>
2020-06-24 10:33:30 +0530

    corrected data preprocessing

--- Data preprocessing/data_preprocessing_template.py
+++ Data preprocessing/data_preprocessing_template.py
@@ -11,13 +11,13 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
 # taking care of missing data
-"""from sklearn.impute import SimpleImputer
+from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
 imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])"""
+X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 # Encoding categorical data
-"""from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 #Encode Country Column
 labelencoder_X = LabelEncoder()
@@ -25,16 +25,16 @@ X[:,0] = labelencoder_X.fit_transform(X[:,0])
 ct = ColumnTransformer([("Country", OneHotEncoder(), [0])], remainder = 'passthrough')
 X = ct.fit_transform(X)  
 labelencoder_y = LabelEncoder()
-y = labelencoder_y.fit_transform(y)"""
+y = labelencoder_y.fit_transform(y)
 
 # Splitting the dataset into train and test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=0)
 
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)"""
+X_test = sc_X.transform(X_test)
 
 
